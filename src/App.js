5f5362a8698497dbfe52abf809080ea814377301
ChironJon <immrwhiskers4@gmail.com>
2020-06-23 02:45:23 -0500

    adding stuff back in

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import insects from './components/insects';
 import CardList from './components/CardList/CardList';
 
 import './App.css';
-//https://drive.google.com/file/d/1rWmXZNuh-ae3DMBhDNSUtLwzYksLDwUG/view?usp=sharing
+
 const insectList = insects.map((item) => item);
 class App extends Component {
   constructor(props) {
