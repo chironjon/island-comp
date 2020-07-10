daa96674e6f2ba5687bf7d90abbced83a237542d
ChironJon <immrwhiskers4@gmail.com>
2020-07-09 23:11:17 -0500

    fix text

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -7,7 +7,7 @@ const UserInput = ({ onButtonPress, sliderMonthChange, sliderHourChange, time })
 			<div>
 	      ↓↓↓Working↓↓↓<p>Month: {time.month} and Hour: {time.hour}</p>
   			↓↓↓Mostly Working↓↓↓<SliderInput  sliderMonthChange={sliderMonthChange} sliderHourChange={sliderHourChange} time={time}/>
-  			Working ==><button onClick={onButtonPress}>RIGHT NOW</button>Doesn't update after pressing "RIGHT NOW"
+  			Working ==><button onClick={onButtonPress}>RIGHT NOW</button>Doesn't update sliders after pressing "RIGHT NOW"
 			</div>
 		)
 
